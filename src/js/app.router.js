198f70e2c8a33344d5ebe6d1c8d1f6dfece24a71
milkmidi <milkmidi@gmail.com>
2017-07-22 08:25:03 +0800

    update vue-loader versrion, use es6 import, not support require *.vue

--- src/js/app.router.js
+++ src/js/app.router.js
@@ -3,19 +3,20 @@ import VueRouter from 'vue-router';
 import Vue from 'vue';
 import store from './store/store';
 
+import Index from '../component/Index.vue';
+
 Vue.use(VueRouter);
+const About = () => import(/* webpackChunkName: "About" */'../component/About.vue');
+const Login = () => import(/* webpackChunkName: "Login" */'../component/Login.vue');
 
 const log = value => console.log(`%c${value}`, 'background: #bdc3c7; color: black; font-size:10px;');
 
 
-const About = () => import(/* webpackChunkName: "About" */'../component/About.vue');
-const Login = () => import(/* webpackChunkName: "Login" */'../component/Login.vue');
-
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: require('../component/Index.vue') },
-    { path: '/index', component: require('../component/Index.vue') },
+    { path: '/', component: Index },
+    { path: '/index', component: Index },
     { path: '/about', component: About, meta: { authorization: true } },
     { path: '/login', component: Login },
   ],
