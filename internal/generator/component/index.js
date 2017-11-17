d443ab751efbc44335a7b52830040950ba0091a6
milkmidi <milkmidi@gmail.com>
2017-11-17 16:40:39 +0800

    update generator

--- internal/generator/component/index.js
+++ internal/generator/component/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { componentExists } = require('../util');
 
+const componentPath = path.resolve('src/js/component');
 module.exports = {
   description: 'add Component',
   prompts: [
@@ -13,12 +14,25 @@ module.exports = {
         if (!/^[A-Za-z][A-Za-z0-9]+$/.test(value)) return 'Invalidte component name!';
         return componentExists(value) ? 'Component name already exists!' : true;
       },
-    }],
+    },
+  ],
   actions: () => [
     {
       type: 'add',
-      path: `${path.resolve('src/js/component')}/{{properCase name}}.vue`,
-      templateFile: 'generator/component/vue.hbs',
+      path: `${componentPath}/{{properCase name}}/{{properCase name}}.vue`,
+      templateFile: 'component/vue.hbs',
+      abortOnFail: true,
+    },
+    {
+      type: 'add',
+      path: `${componentPath}/{{properCase name}}/index.js`,
+      templateFile: 'component/index.hbs',
+      abortOnFail: true,
+    },
+    {
+      type: 'add',
+      path: `${componentPath}/{{properCase name}}/{{properCase name}}.stories.js`,
+      templateFile: 'component/stories.hbs',
       abortOnFail: true,
     },
   ],
--- internal/generator/container/index.js
+++ internal/generator/container/index.js
@@ -1,22 +1,33 @@
 const path = require('path');
 const { containerExists } = require('../util');
 
+const containerPath = path.resolve('src/js/container');
 module.exports = {
   description: 'add Container',
-  prompts: [{
-    type: 'input',
-    name: 'name',
-    message: 'What should it be called',
-    default: 'Button',
-    validate(value) {
-      if (!/^[A-Za-z][A-Za-z0-9]+$/.test(value)) return 'Invalidte component name!';
-      return containerExists(value) ? 'Component name already exists!' : true;
+  prompts: [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What should it be called',
+      default: 'Button',
+      validate(value) {
+        if (!/^[A-Za-z][A-Za-z0-9]+$/.test(value)) return 'Invalidte component name!';
+        return containerExists(value) ? 'Container name already exists!' : true;
+      },
     },
-  }],
-  actions: () => [{
-    type: 'add',
-    path: `${path.resolve('src/js/container')}/{{properCase name}}.vue`,
-    templateFile: 'generator/container/vue.hbs',
-    abortOnFail: true,
-  }],
+  ],
+  actions: () => [
+    {
+      type: 'add',
+      path: `${containerPath}/{{properCase name}}/{{properCase name}}.vue`,
+      templateFile: 'container/vue.hbs',
+      abortOnFail: true,
+    },
+    {
+      type: 'add',
+      path: `${containerPath}/{{properCase name}}/index.js`,
+      templateFile: 'component/index.hbs',
+      abortOnFail: true,
+    },
+  ],
 };
